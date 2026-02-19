b6e50ab60a2c9c84b5c1c2b18359484674f6a63e
Dgpro5 <nicolococuzza2000@gmail.com>
2026-02-19 21:45:28 +0100

    Changed USDC Polygon address to USDC to USDC.e (make sure to have USDC.e in your wallet, and NOT USDC)

--- src/main.rs
+++ src/main.rs
@@ -165,7 +165,7 @@ struct CreateOrderRequest {
 }
 
 // USDC contract on Polygon (6 decimals)
-const USDC_POLYGON: &str = "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359";
+const USDC_POLYGON: &str = "0x2791bca1f2de4661ed88a30c99a7a9449aa84174";
 const ANKR_API_KEY_ENV: &str = "ANKR_API_KEY";
 
 // Polymarket CTF Exchange on Polygon (non-neg-risk markets)
